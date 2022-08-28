6955386a8556cc2a71fa8f4d134fc036af6d68bf
Cazpian Levén <cazpians@gmail.com>
2022-08-29 00:59:38 +0200

    Added services and .env file

---
+++ src/services/MoviesAPI.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+
+axios.defaults.baseURl = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_API_KEY
+
+/**
+ * GET an endpoint
+ * 
+ * @param {string} endpoint
+ * @returns Promise
+ */
+const get = async (endpoint) => {
+    const res = await axios.get(endpoint)
+
+    return res.data
+}
+
+/**
+ * 
+ * Movies
+ * 
+ */
+
+/**
+ * Get movie 
+ */
+const getMovie = async (id) => {
+    return get(`/movie/${id}?api_key=${API_KEY}&language=en-US`)
+}
+
+/**
+ * Get movie genres
+ */
+const getGenresList = async () => {
+    return get(`/genre/tv/list?api_key=${API_KEY}&language=en-US`)
+}
+
+/**
+ * Get movies based on genre
+ */
+const getMoviesByGenre = async ({queryKey}) => {
+    const [_key, {page, genre_id}] = queryKey
+    return get(`/discover/movie?api_key=${API_KEY}&language=en-US&popularity.desc&with_genres=${genre_id}&include_adult=false&page=${page}`)
+}
+
+/**
+ * Get top rated 
+ */
+const getTopRated = async () => {
+    return get(`/movie/top_rated?api_key=${API_KEY}&language=en-US`)
+}
+
+/**
+ * Get now playing
+ */
+const getNowPlaying = async () => {
+    return get(`/movie/now_playing?api_key=${API_KEY}&language=en-US`)
+}
+
+/**
+ * Get trending movies this week
+ */
+const getTrending = async () => {
+    return get(`/trending/movie/week?api_key=${API_KEY}&language=en-US`)
+}
+
+/**
+ * Get credits
+ */
+const getCredits = async (id) => {
+    return get(`/movie/${id}/credits?api_key=${API_KEY}&language=en-US`)
+}
